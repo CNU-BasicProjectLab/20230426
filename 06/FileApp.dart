4bdf1b2aae343a55b6aceda97d0e0c9555a2caa0
picbuddy <picbuddy@naver.com>
2023-05-02 14:42:18 +0900

    06

---
+++ 06/FileApp.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class FileApp extends StatefulWidget {
+  const FileApp({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _FileApp();
+}
+
+class _FileApp extends State<FileApp> {
+  List<String> itemList = List.empty(growable: true);
+  TextEditingController controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    initData();
+  }
+
+  void initData() async {
+    var result = await readListFile(context);
+    setState(() {
+      itemList.addAll(result);
+    });
+  }
+
+  void writeFileTest(String str) async {
+    var dir = await getApplicationDocumentsDirectory();
+    var file = await File('${dir.path}/fileTest.txt').readAsString();
+    file = file + '\n' + str;
+    File('${dir.path}/fileTest.txt').writeAsStringSync(file);
+  }
+
+  Future<List<String>> readListFile(BuildContext context) async {
+    List<String> items = List.empty(growable: true);
+
+    // inner directory check
+    var key = 'First';
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    bool? firstCheck = pref.getBool(key);
+    var dir = await getApplicationDocumentsDirectory();
+    bool fileExist = await File('${dir.path}/fileTest.txt').exists();
+
+    if (firstCheck == null || firstCheck == false || fileExist == false) {
+      pref.setBool(key, true);
+      try {
+        var file = await DefaultAssetBundle.of(context)
+            .loadString('repo/fileTest.txt');
+        var array = file.split('\n');
+        for (var item in array) {
+          items.add(item);
+        }
+      } catch (e) {
+        print(e);
+      }
+      return items;
+    } else {
+      var file = await File('${dir.path}/fileTest.txt').readAsString();
+      var array = file.split('\n');
+      for (var item in array) {
+        print(item);
+        items.add(item);
+      }
+      return itemList;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('File Example'),
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              TextField(
+                controller: controller,
+                keyboardType: TextInputType.text,
+              ),
+              Expanded(
+                  child: ListView.builder(
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: Center(
+                      child: Text(
+                        itemList[index],
+                        style: const TextStyle(fontSize: 30),
+                      ),
+                    ),
+                  );
+                },
+                itemCount: itemList.length,
+              ))
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            writeFileTest(controller.value.text);
+            setState(() {
+              itemList.add(controller.value.text);
+            });
+          },
+          child: const Icon(Icons.add),
+        ));
+  }
+}
